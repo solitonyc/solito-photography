9b62769d56f1fa945115944dffc43943a1fb4e5c
Solito Reyes II <solitonyc@gmail.com>
2021-01-28 23:28:20 -0500

    home page welcome and link to ecommerce site

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Solito's Photography Website.</h1>
-      <h2>COMING SOON!</h2>
+      <h1>Welcome to Solito's Photography!</h1>
+      <h2>This site is still in development.</h2>
+      <h3>But you can still...</h3>
+      <a target="_blank" href="https://studio.solitophotography.com">Buy Solito's Photography and Wall Art here.</a>
     </div>
   );
 }
