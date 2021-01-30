2787e0b5958b7e3aa519aa6859b2ed6b013a732d
Solito Reyes II <solitonyc@gmail.com>
2021-01-30 18:08:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,32 +6,7 @@ import About from './sections/About';
 import Contact from './sections/Contact';
 import Footer from './sections/Footer';
 
-<<<<<<< HEAD
-class App extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0)
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Main />
-        <Photos />
-        <About />
-        <Contact />
-        <Footer />
-      
-      </div>
-    );
-  }
-=======
-function App() {
-  return (
-    <div className="App">
-      <p>HELLO</p>
-    </div>
-  );
->>>>>>> parent of 5eb20aa... added welcome message to home page
+
 }
 
 export default App;
