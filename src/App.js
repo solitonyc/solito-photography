5386b64b895dccc4d751946812d8a994953fa25b
Solito Reyes II <solitonyc@gmail.com>
2021-01-30 18:23:04 -0500

    THIS IS WHERE I PRETTY MUCH DELETED EVERYTHING AND WENT BACK TO A BOILERPLATE APP and considering just doing a normal 4 page site

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 
-import Photos from './sections/Photos';
-import About from './sections/About';
-import Contact from './sections/Contact';
-import Footer from './sections/Footer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Main />
-        <Photos />
-        <About />
-        <Contact />
-        <Footer />
+       <h1>
+         Hello
+       </h1>
       
       </div>
     );
   }
 
-=======
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <p>HELLO</p>
-    </div>
-  );
->>>>>>> parent of 5eb20aa... added welcome message to home page
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
 import './index.css';
 import App from './App';
-import Photography from './sections/Photography';
-import Photographyflex from './sections/Photographyflex';
-import Brand from './sections/Brand';
 
 
 ReactDOM.render(
-    <Router>
-            <Switch>
-                {/* <App /> */}
-                <Route path="/" exact component={App} />
-                <Route path="/photography" exact component={Photography} />
-                <Route path="/photographyflex" exact component={Photographyflex} />
-                <Route path="/brand" component={Brand} />
-            </Switch>    
-    </Router>,
-    document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
