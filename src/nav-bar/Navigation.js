54e2d13a04fde597fcfba455140ff93d0bd686ad
Solito Reyes II <solitonyc@gmail.com>
2021-01-30 19:12:57 -0500

    add navigation to nav folder on top of this boilplate structure

---
+++ src/nav-bar/Navigation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the NAVIGATION section</h1>
+            </div>
+        )
+    }
+}
