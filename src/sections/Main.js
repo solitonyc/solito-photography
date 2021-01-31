9ea49a1557e732fb9649eb148fd4dd45c16b2582
Solito Reyes II <solitonyc@gmail.com>
2021-01-30 22:24:46 -0500

    added main section content

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -5,7 +5,11 @@ export default class Main extends Component {
     render() {
         return (
             <div className='main_section' id='main'>
-                <h1>This is the MAIN section</h1>
+                <div className='main_content_container'>
+                    <h1>Welcome to Solito's Photography Website</h1>
+                    <h3>This site is currently Under Construction.</h3>
+                    <a target="_blank" href='studio.solitophotography.com'>BUY PHOTOS &amp; WALL ART</a>
+                </div>
             </div>
         )
     }
