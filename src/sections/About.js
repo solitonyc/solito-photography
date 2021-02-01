df6f2d397bda50c746a69e5f9bd0093ea2bf932a
Solito Reyes II <solitonyc@gmail.com>
2021-01-31 20:44:44 -0500

    about page background, styling and font

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './about.css';
+import mainBgImage from './images-sections/bg-03.jpg';
 
 export default class About extends Component {
     render() {
+        let style = {
+            backgroundImage: `url(${mainBgImage})`
+        }
         return (
             <div className='about_section' id='about'>
-                <h1>This is the ABOUT section</h1>
+                <div style={style} className='main_section_background'>
+                    <div className='about_section_content_container'>
+                        <div className='about_headline'>ABOUT ME <br />COMING SOON</div>
+                    </div>   
+                </div>             
             </div>
         )
     }
--- src/sections/Photogallery.js
+++ src/sections/Photogallery.js
@@ -3,11 +3,14 @@ import './photogallery.css';
 
 export default class Photogallery extends Component {
     render() {
+   
         return (
             <div className='pg_section' id='photogallery'>
+                
                 <div className='pg_section_content_container'>
-                    <div className='pg_headline'>This is the PHOTOGALLERY section</div>
-                </div>                
+                    <div className='pg_headline'>PHOTOGALLERY <br />COMING SOON</div>
+                </div>  
+                             
             </div>
         )
     }
