991d6faf0fd5063c89a5e38375019a83d20f65e3
Solito Reyes II <solitonyc@gmail.com>
2021-02-01 15:57:16 -0500

    photos for photogallery, favicon, photogallery grid implemented

--- src/sections/Photogallery.js
+++ src/sections/Photogallery.js
@@ -1,14 +1,130 @@
 import './photogallery.css';
 import React, { Component } from 'react';
 
+import PhotoImage001 from '../sections/images-photogallery/a_img.jpg';
+import PhotoImage002 from '../sections/images-photogallery/b_img.jpg';
+import PhotoImage003 from '../sections/images-photogallery/c_img.jpg';
+import PhotoImage004 from '../sections/images-photogallery/d_img.jpg';
+import PhotoImage005 from '../sections/images-photogallery/e_img.jpg';
+import PhotoImage006 from '../sections/images-photogallery/f_img.jpg';
+import PhotoImage007 from '../sections/images-photogallery/g_img.jpg';
+import PhotoImage008 from '../sections/images-photogallery/h_img.jpg';
+import PhotoImage009 from '../sections/images-photogallery/i_img.jpg';
+import PhotoImage010 from '../sections/images-photogallery/j_img.jpg';
+import PhotoImage011 from '../sections/images-photogallery/k_img.jpg';
+import PhotoImage012 from '../sections/images-photogallery/l_img.jpg';
+import PhotoImage013 from '../sections/images-photogallery/m_img.jpg';
+import PhotoImage014 from '../sections/images-photogallery/n_img.jpg';
+import PhotoImage015 from '../sections/images-photogallery/o_img.jpg';
+import PhotoImage016 from '../sections/images-photogallery/p_img.jpg';
+import PhotoImage017 from '../sections/images-photogallery/q_img.jpg';
+import PhotoImage018 from '../sections/images-photogallery/r_img.jpg';
+import PhotoImage019 from '../sections/images-photogallery/s_img.jpg';
+import PhotoImage020 from '../sections/images-photogallery/t_img.jpg';
+import PhotoImage021 from '../sections/images-photogallery/u_img.jpg';
+import PhotoImage022 from '../sections/images-photogallery/v_img.jpg';
+import PhotoImage023 from '../sections/images-photogallery/w_img.jpg';
+import PhotoImage024 from '../sections/images-photogallery/x_img.jpg';
+import PhotoImage025 from '../sections/images-photogallery/y_img.jpg';
+import PhotoImage026 from '../sections/images-photogallery/z_img.jpg';
+import PhotoImage027 from '../sections/images-photogallery/a1_img.jpg';
+import PhotoImage028 from '../sections/images-photogallery/b1_img.jpg';
+
 
 export default class Photogallery extends Component {
     render() {   
         return (
-            <div className='pg_section' id='photogallery'>
-                <div className='pg_section_content_container'>
-                    <div className='pg_headline'>PHOTOGALLERY <br />COMING SOON</div>
-                </div>                               
+            <div className='pg_grid_section'>
+                <div className='pg_grid_header'>PHOTO  GALLERY</div>
+                <div className='pg_grid_secondline'>By SOLITO REYES II</div>
+                <div className='pg_grid_container'>
+                    <div className='pg_grid_box p-1'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage001} />
+                    </div>
+                    <div className='pg_grid_box p-2'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage002} />
+                    </div>
+                    <div className='pg_grid_box p-3'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage003} />
+                    </div>
+                    <div className='pg_grid_box p-4'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage004} />
+                    </div>
+                    <div className='pg_grid_box p-5'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage005} />
+                    </div>
+                    <div className='pg_grid_box p-6'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage006} />
+                    </div>
+                    <div className='pg_grid_box p-7'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage007} />
+                    </div>                   
+                    <div className='pg_grid_box p-8'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage008} />
+                    </div>
+                    <div className='pg_grid_box p-9'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage009} />
+                    </div>
+                    <div className='pg_grid_box p-10'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage010} />
+                    </div>
+                    <div className='pg_grid_box p-11'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage011} />
+                    </div>                   
+                    <div className='pg_grid_box p-12'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage012} />
+                    </div>
+                    <div className='pg_grid_box p-13'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage013} />
+                    </div>
+                    <div className='pg_grid_box p-14'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage014} />
+                    </div>
+                    <div className='pg_grid_box p-15'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage015} />
+                    </div>
+                    <div className='pg_grid_box p-16'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage016} />
+                    </div>                   
+                    <div className='pg_grid_box p-17'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage017} />
+                    </div>
+                    <div className='pg_grid_box p-18'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage018} />
+                    </div>
+                    <div className='pg_grid_box p-19'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage019} />
+                    </div>                   
+                    <div className='pg_grid_box p-20'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage020} />
+                    </div>
+                    <div className='pg_grid_box p-21'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage021} />
+                    </div>
+                    <div className='pg_grid_box p-22'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage022} />
+                    </div>
+                    <div className='pg_grid_box p-23'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage023} />
+                    </div>                   
+                    <div className='pg_grid_box p-24'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage024} />
+                    </div>
+                    <div className='pg_grid_box p-25'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage025} />
+                    </div>
+                    <div className='pg_grid_box p-26'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage026} />
+                    </div>
+                    <div className='pg_grid_box p-27'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage027} />
+                    </div>                   
+                    <div className='pg_grid_box p-28'>
+                        <img className='pg_grid_image' alt='' src={PhotoImage028} />
+                    </div>
+
+                    
+                </div>           
             </div>
         )
     }
