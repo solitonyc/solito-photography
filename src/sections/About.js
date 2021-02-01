407ad8282a71e7c99cd199f9f6d3d9ae26535afb
Solito Reyes II <solitonyc@gmail.com>
2021-01-31 23:42:29 -0500

    about me content - first draft

--- src/sections/About.js
+++ src/sections/About.js
@@ -12,7 +12,17 @@ export default class About extends Component {
             <div className='about_section' id='about'>
                 <div style={style} className='main_section_background'>
                     <div className='about_section_content_container'>
-                        <div className='about_headline'>ABOUT ME <br />COMING SOON</div>
+                        <div className='about_headline'>ABOUT ME</div>
+                        <div className="about_copy">
+                            An urban photographer who gets enthralled by the overwhelming power of the skylines 
+                            that surround him. A former Manhattan nightclub promoter who continues to see
+                            energy, rhythm and inspiration within the beauty that surrounds him in the urban 
+                            lanscape of where he lives. Cityscapes serve as grand symbols of what humans are 
+                            capable of achieving and building 
+                            with their own hands. And within these constructs of grandeur lie humanity's embodiment 
+                            in colorful, cultural displays of art, food, dance, fashion and communication. No 
+                            matter where you are, I hope you find inspiration in these images of urban aesthetica.
+                        </div>
                     </div>   
                 </div>             
             </div>
