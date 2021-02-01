d9ad5ecfa7c34f8bf6ff78a3ffa574655d19eff1
Solito Reyes II <solitonyc@gmail.com>
2021-02-01 08:36:31 -0500

    main page tweaks with spacing copywrite and button colors

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -13,8 +13,8 @@ export default class Main extends Component {
                 <div style={style} className='main_section_background'>
                     <div className='main_section_content_container'>
                         <div className="main_headline">SOLITO PHOTOGRAPHY </div>
-                        <div className='main_headline_2'>Site is currently Under Construction.</div>
-                        <div className='main_copy'>You can still buy photos alone <br />or framed as wall decor:</div>
+                        <div className='main_headline_2'>Site is Under Construction</div>
+                        <div className='main_copy'>You can still buy photos alone <br />or as framed, wall art.</div>
                         <Link to={{ pathname: 'studio.solitophotography.com' }} className='main_button' target="_blank" >Shop Photography</Link>
                     </div>
                 </div>
