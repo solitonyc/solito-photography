1a1ed6134bad6f52823996d28e04e309ede5ce41
Solito Reyes II <solitonyc@gmail.com>
2021-02-04 23:00:23 -0500

    buy button copy tweak, headline coloring

--- src/sections/Buy.js
+++ src/sections/Buy.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './buy.css';
+import './common-styles.css';
 import {Link} from 'react-router-dom';
 
 export default class Buy extends Component {
@@ -9,7 +10,7 @@ export default class Buy extends Component {
                 <div className='buy_content_container'>
                     <div className='buy_headline'>Browse My Photography Store</div>
                     <div className='buy_copy'>Build your collection of urban photography, with cities such as Manhattan. You are provided the option of buying high-resolution prints alone; or as customized wall art. For wall art, select your size, print medium and framing that best suits your decor and preferences.</div>
-                    <div className='buy_copy'>Click this button to see the online store:</div>
+                    <div className='buy_button_copy'>Click this button to see the online store:</div>
                     <Link to={{ pathname: 'https://studio.solitophotography.com' }} rel='noreferrer' className='common_button' target="_blank" >Buy Photography</Link>
                 </div>
                     
