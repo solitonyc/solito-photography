b6c3ab074eebebbea53534c783e34833929416dc
Solito Reyes II <solitonyc@gmail.com>
2021-02-04 22:40:41 -0500

    uploaded new main page logo and tweaked main page content and styling

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -3,6 +3,7 @@ import './common-styles.css';
 import React, { Component } from 'react';
 import mainBgImage from './images-sections/bg-01.jpg';
 import {Link} from 'react-router-dom';
+import bigLogo from './images-sections/sr-logo.png';
 
 export default class Main extends Component {
      render() {
@@ -12,10 +13,9 @@ export default class Main extends Component {
         return (
             <div className='main_section' id='main'>
                 <div style={style} className='main_section_background'>
-                    <div className='main_section_content_container'>
-                        <div className="main_headline">SOLITO REYES Photography </div>
-                        <div className='main_headline_2'>Site is Under Construction</div>
-                        <div className='main_copy'>You can still buy photos alone <br />or as framed, wall art.</div>
+                    <div className='main_content_container'>
+                        <img className='big_logo' alt='' src={bigLogo} />                                            
+                        <div className='main_copy'>See store to shop photos alone or as framed, wall art:</div>
                         <Link to={{ pathname: 'https://studio.solitophotography.com' }} rel='noreferrer' className='common_button' target="_blank" >Buy Photography</Link>
                     </div>
                 </div>
